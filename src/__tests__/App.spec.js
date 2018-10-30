92d931f797fd335e07d8223606286142a1017e59
Kevin Killingsworth <kevin@redfenix.com>
2018-10-30 13:09:43 -0500

    Fix App render tests

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,9 +1,11 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
 it( 'renders without crashing', () => {
 	const div = document.createElement( 'div' );
-	ReactDOM.render( <App />, div);
+	const store = { getState: jest.fn(), dispatch: jest.fn(), subscribe: jest.fn () };
+	ReactDOM.render( <App store={ store } />, div);
 	ReactDOM.unmountComponentAtNode( div );
 } );
