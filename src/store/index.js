d9dc9f549655297db8e6f08163f29f8fd005ac20
Kevin Killingsworth <kevin@redfenix.com>
2018-10-30 10:54:14 -0500

    Separate createStore for prod/dev
    This separates out createStore to make it more obvious that the only
    enhancer is for dev.

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,10 @@
 import { createStore } from 'redux';
 
 export default ( reducer ) => {
-	let enhancer = null;
-
 	if ( 'development' === process.env.NODE_ENV && window.__REDUX_DEVTOOLS_EXTENSION__ ) {
-		enhancer = window.__REDUX_DEVTOOLS_EXTENSION__();
+		const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__();
+		return createStore( reducer, undefined, enhancer );
 	}
 
-	// TODO: Preload state from offline data?
-	return createStore(
-		reducer,
-		{},
-		enhancer
-	);
+	return createStore( reducer );
 }
